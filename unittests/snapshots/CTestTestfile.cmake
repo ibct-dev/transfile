ae41fd4b3942ffb9f75c27af5c1e064bae3bc83a
xter <xter@ibct.kr>
2024-04-11 15:37:38 +0900

    agency

--- unittests/snapshots/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev001/LEDGIS/unittests/snapshots
-# Build directory: /home/dev001/LEDGIS/build/unittests/snapshots
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- unittests/test-contracts/get_table_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dev001/LEDGIS/unittests/test-contracts/get_table_test
-# Build directory: /home/dev001/LEDGIS/build/unittests/test-contracts/get_table_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
