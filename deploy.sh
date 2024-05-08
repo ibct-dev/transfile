0e8f2391e757fdca91ad9ed38a4689c43560ef63
xter <xter@ibct.kr>
2024-05-08 14:58:48 +0900

    did-lit-api

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+tar -cvf dist.tar ../did-lit-api/dist
+tar -cvf node_modules.tar ../did-lit-api/node_modules
+
+# 압축할 원본 파일
+input_file="node_modules.tar"
+
+# 60MB 단위로 분할
+split -b 400m "$input_file" "chunk_"
+
+# 분할된 파일들을 gzip으로 압축
+for chunk in chunk_*; do
+  gzip "$chunk"
+done
+
+rm node_modules.tar
