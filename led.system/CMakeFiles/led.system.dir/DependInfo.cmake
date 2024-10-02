3730fd0c6dc297fd5120323cb14483e91c569066
Cloud User <ubuntu@ibct-did-testnet-bp-node.novalocal>
2024-10-02 12:36:38 +0900

    add led.system

---
+++ led.system/CMakeFiles/led.system.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/delegate_bandwidth.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/delegate_bandwidth.cpp.obj"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/exchange_state.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/exchange_state.cpp.obj"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/led.system.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/led.system.cpp.obj"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/native.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/native.cpp.obj"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/producer.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/producer.cpp.obj"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/src/producer_pay.cpp" "/home/ubuntu/led.public.contracts/build/contracts/led.system/CMakeFiles/led.system.dir/src/producer_pay.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/ubuntu/led.public.contracts/contracts/led.system/include"
+  "/home/ubuntu/led.public.contracts/contracts/led.system/../led.token/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
